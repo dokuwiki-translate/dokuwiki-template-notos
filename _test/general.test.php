f3b138fddda7c24af698c6741d3c0c3b823fb0a5
Anna Dabrowska <dabrowska@cosmocode.de>
2021-07-05 15:55:20 +0200

    Add simple navigation test

---
+++ _test/general.test.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @group tpl_notos
+ * @group plugins
+ */
+class general_tpl_notos_test extends DokuWikiTest
+{
+
+    /**
+     * Simple test to make sure the template.info.txt is in correct format
+     */
+    public function test_templateinfo()
+    {
+        $file = __DIR__ . '/../template.info.txt';
+        $this->assertFileExists($file);
+
+        $info = confToHash($file);
+
+        $this->assertArrayHasKey('base', $info);
+        $this->assertArrayHasKey('author', $info);
+        $this->assertArrayHasKey('email', $info);
+        $this->assertArrayHasKey('date', $info);
+        $this->assertArrayHasKey('name', $info);
+        $this->assertArrayHasKey('desc', $info);
+        $this->assertArrayHasKey('url', $info);
+
+        $this->assertEquals('notos', $info['base']);
+        $this->assertRegExp('/^https?:\/\//', $info['url']);
+        $this->assertTrue(mail_isvalid($info['email']));
+        $this->assertRegExp('/^\d\d\d\d-\d\d-\d\d$/', $info['date']);
+        $this->assertTrue(false !== strtotime($info['date']));
+    }
+}
---
+++ _test/navigation.test.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @group tpl_notos
+ * @group plugins
+ */
+class navigation_tpl_notos_test extends DokuWikiTest
+{
+
+    /**
+     * Simple test to make sure the template.info.txt is in correct format
+     */
+    public function test_templateinfo()
+    {
+        $file = __DIR__ . '/navigation.txt';
+
+        $templateController = new \dokuwiki\template\twigstarter\TemplateController('main');
+        $customController = new \dokuwiki\template\notos\CustomController($templateController);
+
+        $actual = $customController->parseNavigation($file);
+
+        $expected = [
+            0 =>
+                [
+                    'type' => 'internal',
+                    'page' => 'briefs:start',
+                    'title' => 'Briefs',
+                ],
+            1 =>
+                [
+                    'type' => 'internal',
+                    'page' => 'qhsr:start',
+                    'title' => 'QHSR',
+                    'sub' =>
+                        [
+                            0 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:q',
+                                    'title' => 'Quality',
+                                ],
+                            1 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:cert',
+                                    'title' => 'Certification',
+                                ],
+                            2 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:hse:start',
+                                    'title' => 'HSE',
+                                ],
+                            3 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:engsystems',
+                                    'title' => 'Eng Systems',
+                                ],
+                            4 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:performance',
+                                    'title' => 'Eng Performance',
+                                ],
+                            5 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:competence',
+                                    'title' => 'Eng Competence',
+                                ],
+                            6 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:ashford',
+                                    'title' => 'Ashford DFO',
+                                ],
+                            7 =>
+                                [
+                                    'type' => 'internal',
+                                    'page' => 'qhsr:training',
+                                    'title' => 'Technical Training',
+                                ],
+                        ],
+                ],
+            2 =>
+                [
+                    'type' => 'internal',
+                    'page' => 'tech:start',
+                    'title' => 'Tech Info',
+                ],
+            3 =>
+                [
+                    'type' => 'external',
+                    'page' => 'https://homepage.company.com',
+                    'title' => 'Company Homepage',
+                ],
+        ];
+
+        $this->assertEquals($expected, $actual);
+    }
+}
