0a8e8f06dbd60066c1d73827412deaac6c55b75c
Anna Dabrowska <dabrowska@cosmocode.de>
2021-07-07 15:38:33 +0200

    use ini replacements instead of custom variables

---
+++ lang/en/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['__highlight__']              = 'Highlight';
+$lang['__accent__']                 = 'Accent';
+
+$lang['__header_background__']      = 'Header background';
+$lang['__header_text__']            = 'Header text';
+
+$lang['__site_width__']             = 'Site width';
+$lang['__phone_width__']            = 'Mobile view width';
