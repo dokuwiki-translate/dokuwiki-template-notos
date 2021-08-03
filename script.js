b897f7ec3f181891cf194a6646414045cfc0c838
Anna Dabrowska <dabrowska@cosmocode.de>
2021-08-03 16:28:09 +0200

    better JS loading

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/**
- * Mobile menu open handling
- */
 jQuery(function () {
+    /**
+    * Mobile menu open handling
+    */
     const $tabs = jQuery('nav.notos ul.navtabs');
     $tabs.on('click', '.primary .opener', function (event) {
         $tabs.find('li').removeClass('active');
@@ -9,23 +9,23 @@ jQuery(function () {
         event.stopPropagation();
         event.preventDefault();
     });
-});
 
-/**
- * Close other notos toggle menus when opening a new one
- */
-jQuery('input.notos-toggle').on('click', function () {
-    jQuery('input.notos-toggle').not(this).prop('checked', false);
-});
+    /**
+     * Close other notos toggle menus when opening a new one
+     */
+    jQuery('input.notos-toggle').on('click', function () {
+        jQuery('input.notos-toggle').not(this).prop('checked', false);
+    });
 
-/**
- * Close notos toggle menus on clicks elsewhere
- */
-jQuery('body').on('click', function(event) {
-    const $target = jQuery(event.target);
-    if($target.is('.notos-toggle') || $target.parents('.notos-toggle').length) return;
-    const $toggle = jQuery('input.notos-toggle');
-    if ($toggle.prop('checked')) {
-        $toggle.prop('checked', false);
-    }
+    /**
+     * Close notos toggle menus on clicks elsewhere
+     */
+    jQuery('body').on('click', function(event) {
+        const $target = jQuery(event.target);
+        if ($target.is('.notos-toggle') || $target.parents('.notos-toggle').length) return;
+        const $toggle = jQuery('input.notos-toggle');
+        if ($toggle.prop('checked')) {
+            $toggle.prop('checked', false);
+        }
+    });
 });
