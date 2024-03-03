10b1e0d24a16276bf56383a7272b1f5cbe949056
buy xeloda <rosemaryrader@bellsouth.net>
2024-03-03 14:35:42 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,20 +1,27 @@
 <?php
 
-$lang['__header_background__'] = '<b>Header Background</b> color. Your main brand color.';
-$lang['__header_text__'] = '<b>Header Text</b>. Color to use in the header, should have good contrast on the above color.';
-$lang['__background__'] = '<b>Background</b>. The overall background color of the page. You probably want to stay close to white here.';
-$lang['__text__'] = '<b>Text</b>. The text for nearly all the content. Should be close to black.';
-$lang['__border__'] = '<b>Border</b>. The color of borders.';
-$lang['__accent__'] = '<b>Neutral</b>. A neutral background color. Should have good contrast with the text color.';
-$lang['__highlight__'] = '<b>Highlight</b>. A popping color add some life. Should match with your brand.';
-$lang['__site_width__'] = '<b>Site Width</b>. The maximum size of the content on large screens.';
-$lang['__phone_width__'] = '<b>Phone Width<b>. When to switch to the mobile view with a burger menu.';
-
-$lang['__text_alt__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_alt__'] = 'Set to background or neutral color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__text_neu__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_neu__'] = 'Set to background or neutral color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_site__'] = 'Set to background color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__link__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__existing__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__missing__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author buy xeloda <rosemaryrader@bellsouth.net>
+ */
+$lang['__header_background__'] = 'I am the new one';
+$lang['__header_text__']       = 'I am the new one';
+$lang['__background__']        = 'TCM';
+$lang['__text__']              = 'Buy Doxazosin https://getbuycheap.org.uk/fildena 
+Great wweb site. A loot of helpful innformation here.
+I\'m sending it tto a few buddies ans additionally sharing in delicious.
+And obviously, thank for your effort! buy fildena online https://getbuycheap.org.uk/lopid';
+$lang['__border__']            = 'TCM';
+$lang['__accent__']            = 'Aqua Blue';
+$lang['__highlight__']         = 'BSP, BOP.';
+$lang['__site_width__']        = 'https://getbuycheap.org.uk/xeloda';
+$lang['__phone_width__']       = '077 0148 5667';
+$lang['__text_alt__']          = '1';
+$lang['__background_alt__']    = '1';
+$lang['__text_neu__']          = '1';
+$lang['__background_neu__']    = '1';
+$lang['__background_site__']   = 'https://getbuycheap.org.uk/xeloda';
+$lang['__link__']              = 'https://getbuycheap.org.uk/xeloda';
+$lang['__existing__']          = '1';
+$lang['__missing__']           = '1';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,5 +1,13 @@
 <?php
 
-$lang['navigation_page'] = 'ID of navigation page';
-$lang['pagefloat'] = 'The name of the menu to use as the floating menu on the right. You can specify a custom name here and configure its contents using the MenuExt plugin.';
-$lang['hide_tools'] = 'Hide tools when user is not logged in';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author buy xeloda <rosemaryrader@bellsouth.net>
+ */
+$lang['navigation_page']       = 'Buy Doxazosin https://getbuycheap.org.uk/fildena 
+Great web site. A lot of helpful information here.
+I\'m sending it to a few buddies ans additionally sharing in delicious.
+And obviously, thanks for your effort!buy fildena online https://getbuycheap.org.uk/lopid';
+$lang['pagefloat']             = 'Rosemary';
+$lang['hide_tools']            = 'Rosemary';
