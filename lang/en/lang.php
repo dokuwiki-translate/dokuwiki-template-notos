aec19a3ba026ad1ae2d68b3db99f02cd27329fe2
Casinozeus-Ar.Top <garlandmoberg@hotmail.com>
2025-10-17 04:15:43 +0200

    Translation update (en)

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,20 +1,24 @@
 <?php
 
-$lang['__header_background__'] = '<b>Header Background</b> color. Your main brand color.';
-$lang['__header_text__'] = '<b>Header Text</b>. Color to use in the header, should have good contrast on the above color.';
-$lang['__background__'] = '<b>Background</b>. The overall background color of the page. You probably want to stay close to white here.';
-$lang['__text__'] = '<b>Text</b>. The text for nearly all the content. Should be close to black.';
-$lang['__border__'] = '<b>Border</b>. The color of borders.';
-$lang['__accent__'] = '<b>Neutral</b>. A neutral background color. Should have good contrast with the text color.';
-$lang['__highlight__'] = '<b>Highlight</b>. A popping color add some life. Should match with your brand.';
-$lang['__site_width__'] = '<b>Site Width</b>. The maximum size of the content on large screens.';
-$lang['__phone_width__'] = '<b>Phone Width<b>. When to switch to the mobile view with a burger menu.';
-
-$lang['__text_alt__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_alt__'] = 'Set to background or neutral color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__text_neu__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_neu__'] = 'Set to background or neutral color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_site__'] = 'Set to background color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__link__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__existing__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__missing__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Casinozeus-Ar.Top <garlandmoberg@hotmail.com>
+ */
+$lang['__header_background__'] = 'I am the new guy';
+$lang['__header_text__']       = 'I am the new guy';
+$lang['__background__']        = 'Default';
+$lang['__text__']              = 'Thanks for the read. See my related article: .';
+$lang['__border__']            = 'Aqua Blue';
+$lang['__accent__']            = 'Sand black';
+$lang['__highlight__']         = 'Default';
+$lang['__site_width__']        = 'https://casinozeus-ar.top/';
+$lang['__phone_width__']       = '621637000';
+$lang['__text_alt__']          = '1';
+$lang['__background_alt__']    = '1';
+$lang['__text_neu__']          = '1';
+$lang['__background_neu__']    = '1';
+$lang['__background_site__']   = 'https://casinozeus-ar.top/';
+$lang['__link__']              = 'https://casinozeus-ar.top/';
+$lang['__existing__']          = '1';
+$lang['__missing__']           = '1';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,5 +1,10 @@
 <?php
 
-$lang['navigation_page'] = 'ID of navigation page';
-$lang['pagefloat'] = 'The name of the menu to use as the floating menu on the right. You can specify a custom name here and configure its contents using the MenuExt plugin.';
-$lang['hide_tools'] = 'Hide tools when user is not logged in';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Casinozeus-Ar.Top <garlandmoberg@hotmail.com>
+ */
+$lang['navigation_page']       = 'Thanks for the read. See my related article: .';
+$lang['pagefloat']             = 'Garland';
+$lang['hide_tools']            = 'Garland';
