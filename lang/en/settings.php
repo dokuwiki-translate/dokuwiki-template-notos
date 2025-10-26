5119057b2a5634bab47122ea79aeaa6fbcb28905
EBONY PORN <laraepatch@neuf.fr>
2025-10-26 17:50:26 +0100

    Translation update (en)

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,5 +1,10 @@
 <?php
 
-$lang['navigation_page'] = 'ID of navigation page';
-$lang['pagefloat'] = 'The name of the menu to use as the floating menu on the right. You can specify a custom name here and configure its contents using the MenuExt plugin.';
-$lang['hide_tools'] = 'Hide tools when user is not logged in';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author EBONY PORN <laraepatch@neuf.fr>
+ */
+$lang['navigation_page']       = '性内容 在专为成熟观众设计的平台上广泛可用。选择 安全网站 以确保安全。';
+$lang['pagefloat']             = 'Larae';
+$lang['hide_tools']            = 'Larae';
