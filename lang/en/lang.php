2daaf1d9c5e49c4cea38ea6072ba04122410fbb2
cipla sildenafil <martisherrod@live.co.uk>
2025-11-07 19:25:26 +0100

    Translation update (en)

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,20 +1,24 @@
 <?php
 
-$lang['__header_background__'] = '<b>Header Background</b> color. Your main brand color.';
-$lang['__header_text__'] = '<b>Header Text</b>. Color to use in the header, should have good contrast on the above color.';
-$lang['__background__'] = '<b>Background</b>. The overall background color of the page. You probably want to stay close to white here.';
-$lang['__text__'] = '<b>Text</b>. The text for nearly all the content. Should be close to black.';
-$lang['__border__'] = '<b>Border</b>. The color of borders.';
-$lang['__accent__'] = '<b>Neutral</b>. A neutral background color. Should have good contrast with the text color.';
-$lang['__highlight__'] = '<b>Highlight</b>. A popping color add some life. Should match with your brand.';
-$lang['__site_width__'] = '<b>Site Width</b>. The maximum size of the content on large screens.';
-$lang['__phone_width__'] = '<b>Phone Width<b>. When to switch to the mobile view with a burger menu.';
-
-$lang['__text_alt__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_alt__'] = 'Set to background or neutral color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__text_neu__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_neu__'] = 'Set to background or neutral color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__background_site__'] = 'Set to background color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__link__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__existing__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
-$lang['__missing__'] = 'Set to text color. (Not used by Notos, but Plugins may refer to it.)';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author cipla sildenafil <martisherrod@live.co.uk>
+ */
+$lang['__header_background__'] = '我只是想说你好！';
+$lang['__header_text__']       = '我只是想说你好！';
+$lang['__background__']        = 'Sand black';
+$lang['__text__']              = '领先的成人网站 为成熟观众提供优质内容。探索 经过验证的成人网站 以确保质量和隐私。';
+$lang['__border__']            = 'TCM';
+$lang['__accent__']            = 'BSP, BOP.';
+$lang['__highlight__']         = 'Green';
+$lang['__site_width__']        = 'https://www.webopedia.com/crypto-gambling/best-sites/';
+$lang['__phone_width__']       = '3220446040';
+$lang['__text_alt__']          = '1';
+$lang['__background_alt__']    = '1';
+$lang['__text_neu__']          = '1';
+$lang['__background_neu__']    = '1';
+$lang['__background_site__']   = 'https://www.webopedia.com/crypto-gambling/best-sites/';
+$lang['__link__']              = 'https://www.webopedia.com/crypto-gambling/best-sites/';
+$lang['__existing__']          = '1';
+$lang['__missing__']           = '1';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,5 +1,10 @@
 <?php
 
-$lang['navigation_page'] = 'ID of navigation page';
-$lang['pagefloat'] = 'The name of the menu to use as the floating menu on the right. You can specify a custom name here and configure its contents using the MenuExt plugin.';
-$lang['hide_tools'] = 'Hide tools when user is not logged in';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author cipla sildenafil <martisherrod@live.co.uk>
+ */
+$lang['navigation_page']       = '领先的成人网站 为成熟观众提供优质内容。探索 经过验证的成人网站 以确保质量和隐私。';
+$lang['pagefloat']             = 'Marti';
+$lang['hide_tools']            = 'Marti';
