3a0390e9286bb6a192d4eb346e9a33490c7864c7
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:36:44 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['__header_background__'] = '<b>Pozadie hlavičky</b>. Vaša hlavná firemná farba.';
+$lang['__header_text__']       = '<b>Text hlavičky</b>. Farba použitá v hlavičke, mala by mať dobrý kontrast voči farbe vyššie.';
+$lang['__background__']        = '<b>Pozadie</b>. Celková farba pozadia stránky. Pravdepodobne budete chcieť zostať blízko bielej.';
+$lang['__text__']              = '<b>Text</b>. Text takmer celého obsahu. Mal by byť blízko čiernej.';
+$lang['__border__']            = '<b>Orámovanie</b>. Farba orámovaní.';
+$lang['__accent__']            = '<b>Neutrálna</b>. Neutrálna farba pozadia. Mala by mať dobrý kontrast s farbou textu.';
+$lang['__highlight__']         = '<b>Zvýraznenie</b>. Výrazná farba na oživenie. Mala by ladiť s vašou značkou.';
+$lang['__site_width__']        = '<b>Šírka webu</b>. Maximálna veľkosť obsahu na veľkých obrazovkách.';
+$lang['__phone_width__']       = '<b>Šírka telefónu<b>. Kedy prepnúť na mobilné zobrazenie s burger menu.';
+$lang['__text_alt__']          = 'Nastavte na farbu textu. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
+$lang['__background_alt__']    = 'Nastavte na farbu pozadia alebo neutrálnu. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
+$lang['__text_neu__']          = 'Nastavte na farbu textu. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
+$lang['__background_neu__']    = 'Nastavte na farbu pozadia alebo neutrálnu. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
+$lang['__background_site__']   = 'Nastavte na farbu pozadia. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
+$lang['__link__']              = 'Nastavte na farbu textu. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
+$lang['__existing__']          = 'Nastavte na farbu textu. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
+$lang['__missing__']           = 'Nastavte na farbu textu. (Notos ju nepoužíva, ale pluginy sa na ňu môžu odkazovať.)';
---
+++ lang/sk/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['navigation_page']       = 'ID navigačnej stránky';
+$lang['pagefloat']             = 'Názov menu, ktoré sa použije ako plávajúce menu vpravo. Môžete tu zadať vlastný názov a jeho obsah nakonfigurovať pluginom MenuExt.';
+$lang['hide_tools']            = 'Skryť nástroje, keď používateľ nie je prihlásený';
